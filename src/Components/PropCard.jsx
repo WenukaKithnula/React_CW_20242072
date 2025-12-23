a5a12dd36ada2d54962b7de5bf5f694c269d8063
WenukaKithnula <wkithnula@gmail.com>
2025-12-23 11:34:51 +0530

    added a short description to the json data set and made the structer of the new card component

--- src/Components/PropCard.jsx
+++ src/Components/PropCard.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+function PropCard({
+  property,
+  isFavorite,
+  addToFavorites,
+  removeFromFavorites,
+}) {
+  const { id, type, bedrooms, price, description, location, images, added } =
+    property;
 
-const PropCard = ({ property, isFavorite, addToFavorites, removeFromFavorites }) => {
-  const { type, brand, location, added, images, id } = property;
+  const thumbnail = images?.[0];
 
-  const firstImage = images?.[0];
-
-  const handleClick = () => {
+  const handlClick = () => {
     if (isFavorite) {
       removeFromFavorites(id);
     } else {
@@ -19,30 +23,32 @@ const PropCard = ({ property, isFavorite, addToFavorites, removeFromFavorites })
   };
 
   return (
-    <section
-      className="card"
-      draggable="true"
-      onDragStart={handleDragStart}
-      style={{ border: "1px solid #ccc", margin: "10px", padding: "10px", width: "250px" }}
-    >
-      <div className="image">
-        {firstImage && <img src={firstImage} alt={type} style={{ width: "100%" }} />}
-      </div>
-
-      <div className="description">
-        <h4>Type: {type}</h4>
-        <h4>Brand: {brand}</h4>
-        <h4>Location: {location}</h4>
-        <h4>
-          Added: {added.month} {added.day}, {added.year}
-        </h4>
-
-        <button onClick={handleClick}>
-          {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
-        </button>
-      </div>
-    </section>
+    <>
+      <section onDrag={handleDragStart} draggable="true" className="card">
+        <div className="card-body">
+          <div className="Thumbnail">
+            {thumbnail && <img src={thumbnail} alt={type} />}
+          </div>
+          <div className="property-info">
+            <div className="location">{location}</div>
+            <div className="property-type">
+              <div className="type">{type}</div>
+              <div className="bedrooms">{bedrooms}</div>
+            </div>
+            <div className="description">{description}</div>
+            <div className="price-date">
+              <div className="price">{price}</div>
+              <div className="date">
+                Added: {added.month} {added.day}, {added.year}
+              </div>
+            </div>
+            <button onClick={handlClick}>
+              {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   );
-};
-
+}
 export default PropCard;
